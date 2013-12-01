f8a3b848bb55bf2b147145003c3b4520f2426180
Moin Virani <moin.virani@gmail.com>
2013-12-01 13:31:16 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Savingpool::Application.config.secret_key_base = 'b994d10b8c731d679e50030da9573a68d031ff3eff155eee0fd6d1af0ea8e6e6ae3ebddfd2603c44e147953581d11e2d556b3499d51822cb8033254e1e1db8f6'
